55a2b2c93fdecfa98b9a86038394cc0a33991889
Now <now.thongtangsai@gmail.com>
2023-06-29 10:16:33 +0700

    adding the print warning

--- color_text.py
+++ color_text.py
@@ -1,11 +1,16 @@
 class ColorText:
     GREEN = "\033[0;32m"
     RED = "\033[0;31m"
+    YELLOW = "\033[0;33m"
     END = "\033[0m"
 
     @staticmethod
     def print_ok_info(text):
         print(f"{ColorText.GREEN}INFO:{ColorText.END} {text}\n")
+
+    @staticmethod
+    def print_warning_info(text):
+        print(f"{ColorText.YELLOW}WARNING:{ColorText.END} {text}\n")
 
     @staticmethod
     def color_error(text):
